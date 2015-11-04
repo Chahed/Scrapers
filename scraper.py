6eb204a3c1921597012bfa721f339833e76b1865
Hedi Chahed <hedi.chahed.2i@gmail.com>
2015-11-04 18:44:47 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -116,8 +116,8 @@ def date(dat):
 def dateClean(dat):
     d=dat.split(' ')
     months = dict()
-	months = {'01': 'January', '02': 'February', '03': 'March', '04': 'April', '05': 'May', '06': 'June', '07': 'July', '08': 'August', '09': 'September', '10': 'October', '11': 'November', '12': 'December' }
-	date = d[2] + "-" + months[d[1] + "-" + d[0]
+    months = {'01': 'January', '02': 'February', '03': 'March', '04': 'April', '05': 'May', '06': 'June', '07': 'July', '08': 'August', '09': 'September', '10': 'October', '11': 'November', '12': 'December' }
+    date = d[2] + "-" + months[d[1] + "-" + d[0]
     return date
 
 
