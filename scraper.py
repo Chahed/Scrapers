477b61528e6fec2ed1805bee1929192476384a96
Hedi Chahed <hedi.chahed.2i@gmail.com>
2015-11-05 13:31:34 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -44,7 +44,7 @@ def get_numpages(url):
 def suittext(text):
     text=text.replace(", ,","")
     text=text.replace("'","")
-    text=text.replace("\\n"," ")
+    
     text=text.replace("  ","")
     text=text.replace("\\r"," ")
     text=text.replace("[","")
@@ -139,7 +139,7 @@ def scrap_live(url):
     Contract_Type =BeautifulSoup(Table(htmltext,0)).text
     Procurement_Process =suittext(BeautifulSoup(Table(htmltext,1)).text)
     Estimated_Value_TEXT_DESCRIPTION =suittext(BeautifulSoup(Table(htmltext,2)).text)
-    Category =suittext(Table(htmltext,3))
+    Category =suittext(BeautifulSoup(Table(htmltext,3)))
     
     CPV_codes =suittext(BeautifulSoup(Table(htmltext,4)).text).strip(', ')
     Suitable_for_SME =suittext(BeautifulSoup(Table(htmltext,5)).text)
